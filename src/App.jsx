3bea7b596963b1ad3e73f7142c933b30fd4403c8
lupo tobi <daniellupo30@gmail.com>
2024-12-27 11:15:07 -0800

    add Navbar component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+import Navbar from './Components/Navbar'
+
 function App() {
  
 
   return (
     <>
-    
+    <Navbar />
     </>
   )
 }
